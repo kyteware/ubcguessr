0f11cd91c87c0e82dded0c08c6ee2876ce4cb2ec
Kyteware <kyteware.biz@gmail.com>
2025-01-18 09:39:00 -0800

    DONE PINLOGGER

--- pinlogger/server.js
+++ pinlogger/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('node:fs');
+
 const app = express();
 const PORT = 3001;
 
@@ -11,11 +13,23 @@ app.use(express.json());
 let logs = [];
 
 app.post('/api/logs', (req, res) => {
-    console.log("hi");
     const log = req.body;
     logs.push(log);
     console.log(log);
     res.status(201).end()
+
+    fs.appendFile("./pins.txt", JSON.stringify(log) + "\n", err => {
+        if (err) {
+            console.log("its so over noooo: " + err);
+        } else {
+            console.log("log added");
+        }
+    })
+});
+
+app.get('/test', (_req, res) => {
+    console.log("tester hi");
+    res.status(201).end();
 });
 
 // Start server
